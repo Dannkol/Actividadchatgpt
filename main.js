c03a4f9549efbb0fc7e9d71af1ab6850eff4f538
Daniel Manosalva <Manosalvasierra@gmail.com>
2023-04-26 08:15:00 -0500

    feat: :lipstick: Cambiando el estilo

--- main.js
+++ main.js
@@ -11,10 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
         let msg = await api.api(data.pregunta)
         myworker.postMessage({ data: msg});
         myworker.addEventListener("message", (e) => {
-            let respuesta = document.querySelector('.respuesta')
-            respuesta.innerHTML = ''
+            let respuesta = document.querySelectorAll('.respuesta')
+            respuesta[0].innerHTML = ''
+            respuesta[1].innerHTML = ''
             let doc = new DOMParser().parseFromString(e.data, "text/html");
-            respuesta.appendChild(...doc.body.children);
+            respuesta[0].appendChild(...doc.body.children);
+            respuesta[1].innerHTML = e.data
         })
         /* console.log(await api.api(data.pregunta.generations[0].text))  */
     }
